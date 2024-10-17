0cdbf78dc32006bdaae9966325b9e09e78122a82
Elvis Le <elvishaile120@gmail.com>
2024-10-17 22:35:22 +1100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,7 +24,7 @@ export default function SnakeGame() {
   // Game Settings
   const minGameSpeed = 10
   const maxGameSpeed = 15
-  const wallKills = false
+  const wallKills = true
 
   // Game State
   const [gameDelay, setGameDelay] = useState<number>(1000 / minGameSpeed)
